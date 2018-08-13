77f782f405be23e048c278a75864c0f81250bb47
Magireco <513611748@QQ.COM>
2018-08-14 00:32:16 +0800

    HBJ commit

---
+++ config-client-refresh/src/main/java/cn/com/bosssoft/ConfigClientRefreshApplication.java
@@ -0,0 +1,36 @@
+package cn.com.bosssoft;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@EnableEurekaClient
+@EnableDiscoveryClient
+@RestController
+@RefreshScope
+public class ConfigClientRefreshApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConfigClientRefreshApplication.class, args);
+	}
+
+	@Value("${foo}")
+	String foo;
+	
+	@GetMapping("/foo")
+	  public String hello() {
+	    return this.foo;
+	  }
+	
+	@RequestMapping(value = "/hi")
+	public String hi(){
+		return foo;
+	}
+}
